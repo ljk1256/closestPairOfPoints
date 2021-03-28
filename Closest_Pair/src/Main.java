b0bdb7c7eb7274db8a8ad8e848ecc4eacc40e62c
jongmine <joychoe21@icloud.com>
2021-03-29 04:06:51 +0900

    version 1

--- Closest_Pair/src/Main.java
+++ Closest_Pair/src/Main.java
@@ -19,11 +19,31 @@ public class Main {
         closest_pair.find_pair(coordinates);
     }
 
-    public int[] find_pair(int[][] coordinates) {
+    public int[] find_pair(int[][] coordinates) {  // void 형으로 바꿔도 될 것 같음!!
+        int[][] Answer = new int[2][2];
 
 
+        double smallest_distance = Math.min(left_distance, right_distance);
+        Answer = find_smallest_in_center(coordinates, center_point, smallest_distance);
     }
 
+    public int[][] find_smallest_in_center(int[][] coordinates, int[] center_point, double max_distance) {
+        double smallest_distance = max_distance;  // 최소 거리
+        int[][] smallest_points = new int[2][2];
+
+        for (int i = 0; i < coordinates.length; i++)  // 좌표 x의 최대값까지
+            for (int j = i + 1; j < coordinates[0].length; j++) {  // i와 i+1이랑 비교
+                if (coordinates[i][0] <= center_point[0] - max_distance || coordinates[i][0] >= center_point[0] + max_distance)
+                    if (smallest_distance > get_distance(coordinates, i, j)) {
+                        smallest_distance = get_distance(coordinates, i, j);
+                        smallest_points[0][0] = coordinates[i][0];
+                        smallest_points[0][1] = coordinates[i][1];
+                        smallest_points[1][0] = coordinates[j][0];
+                        smallest_points[1][0] = coordinates[j][1];
+                    }
+            }
+        return smallest_points;
+    }
 
     // 두점 사이의 거리를 구하는 공식
     public static double get_distance(int[][] coordinates, int x, int y) {
