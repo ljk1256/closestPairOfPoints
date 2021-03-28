226089af450076fda37255d5b4c0835c63f3ce3e
jongmine <joychoe21@icloud.com>
2021-03-28 12:10:19 +0900

    add random integer to coordinates

--- Closest_Pair/src/Main.java
+++ Closest_Pair/src/Main.java
@@ -4,10 +4,16 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) {
-	    Scanner scanner = new Scanner(System.in);
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-	    int [][] coordinates;
+        int number_of_points = scanner.nextInt();
+        int[][] coordinates = new int[number_of_points][2];
 
+        // 최대 50의 정수 난수를 좌표 (x,y)에 대입
+        for (int i = 0; i < coordinates.length; i++) {
+            coordinates[i][0] = random.nextInt(50);
+            coordinates[i][1] = random.nextInt(50);
+        }
 
     }
 }
