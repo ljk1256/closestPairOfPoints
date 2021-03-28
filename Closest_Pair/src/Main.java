2d0ce7c1d7b3a72f147042028bc3a34133fea71c
jongmine <joychoe21@icloud.com>
2021-03-28 11:23:54 +0900

    add Main method

--- Closest_Pair/src/Main.java
+++ Closest_Pair/src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+import java.util.Random;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+	    int [][] coordinates;
+
 
     }
 }
