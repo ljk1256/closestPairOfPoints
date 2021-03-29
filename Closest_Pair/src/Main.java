c0bf9ab6d75a152bd665f07601f7e570ff463f6f
jongmine <joychoe21@icloud.com>
2021-03-30 00:49:22 +0900

    version 3

--- Closest_Pair/src/Main.java
+++ Closest_Pair/src/Main.java
@@ -7,41 +7,53 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int number_of_points = scanner.nextInt();
+
         start(number_of_points);
     }
 
-    public static void start(int number_of_points){
-        Random random = new Random();
-
-        int[][] coordinates = new int[number_of_points][2];
-        int[][] resultcoordinate = new int[2][2];
-        double result_d;
-
-        for (int i = 0; i < coordinates.length; i++) {
-            coordinates[i][0] = random.nextInt(50);
-            coordinates[i][1] = random.nextInt(50);
-        }
-        Arrays.sort(coordinates);
-
-        resultcoordinate = divideHalf(coordinates, 0, number_of_points);    // 최종 좌표 배열
-        result_d = get_distance(resultcoordinate, 0, 1);      // 최종 좌표 거리
-
-        System.out.printf("좌표는 (%d, %d) , (%d, %d) 거리는 %f 입니다. ", resultcoordinate[0][0], resultcoordinate[0][1], resultcoordinate[1][0], resultcoordinate[1][1], result_d);
+    public static double get_distance(int[][] coordinates, int x1, int x2) {
+        return Math.sqrt(Math.pow(coordinates[x1][0] - coordinates[x2][0], 2) + Math.pow(coordinates[x1][1] - coordinates[x2][1], 2));
     }
 
+    //분할,정복 재귀적 호출
     public static int[][] divideHalf(int[][] coordinates, int left, int right) {
         int[][] Answer = new int[2][2];
-        double min1_distance, min2_distance;
+
+        if (right - left <= 5) {
+            return closestPair(coordinates, left, right);
+        } else {
+            int mid = left + (right - left) / 2;
+            int[][] min1 = divideHalf(coordinates, left, mid);
+            int[][] min2 = divideHalf(coordinates, mid + 1, right);
 
 
-        double smallest_distance = Math.min(min1_distance, min2_distance);
-        int[][] smallest_center_points = find_smallest_in_center(coordinates, center_point, smallest_distance);
-        if (smallest_distance < get_distance(smallest_center_points, 0, 1))
-            Answer = smallest_center_points.clone();
+            double smallest_distance = Math.min(get_distance(min1, 0, 1), get_distance(min2, 0, 1));
+            int[][] smallest_center_points = find_smallest_in_center(coordinates, center_point, smallest_distance);
+            if (smallest_distance < get_distance(smallest_center_points, 0, 1))
+                Answer = smallest_center_points.clone();
+        }
         return Answer;
     }
 
-    // 중앙에서 최소 거리를 이루는 점의 쌍이 있는지 찾기
+    public static int[][] closestPair(int[][] coordinates, int left, int right) {
+        double min = 10000000;
+        int[][] mincoordinate = new int[2][2];    // 최소거리 좌표 저장 배열
+
+        for (int i = left; i < right + 1; i++) {
+            for (int j = 0; j < right + 1; j++) {
+                double d = get_distance(coordinates, i, j);
+                if (d <= min) {
+                    min = d;
+                    mincoordinate[0][0] = coordinates[i][0];
+                    mincoordinate[0][1] = coordinates[i][1];
+                    mincoordinate[1][0] = coordinates[j][0];
+                    mincoordinate[1][1] = coordinates[j][1];
+                }
+            }
+        }
+        return mincoordinate;
+    }
+
     public static int[][] find_smallest_in_center(int[][] coordinates, int[] center_point, double max_distance) {
         double smallest_distance = max_distance;  // 최소 거리
         int[][] smallest_points = new int[][]{{9999, 9999}, {9999, 9999}};  // 최소 거리를 이루는 좌표쌍
@@ -60,9 +72,23 @@ public class Main {
         return smallest_points;
     }
 
-    // 두점 사이의 거리를 구하는 공식
-    public static double get_distance(int[][] coordinates, int x1, int x2) {
-        return Math.sqrt(Math.pow(coordinates[x1][0] - coordinates[x2][0], 2) + Math.pow(coordinates[x1][1] - coordinates[x2][1], 2));
+    public static void start(int number_of_points) {
+        Random random = new Random();
+
+        int[][] coordinates = new int[number_of_points][2];
+        int[][] resultcoordinate = new int[2][2];
+        double result_d;
+
+        for (int i = 0; i < coordinates.length; i++) {
+            coordinates[i][0] = random.nextInt(50);
+            coordinates[i][1] = random.nextInt(50);
+        }
+        Arrays.sort(coordinates);
+
+        resultcoordinate = divideHalf(coordinates, 0, number_of_points);    // 최종 좌표 배열
+        result_d = get_distance(resultcoordinate, 0, 1);      // 최종 좌표 거리
+
+        System.out.printf("좌표는 (%d, %d) , (%d, %d) 거리는 %f 입니다. ", resultcoordinate[0][0], resultcoordinate[0][1], resultcoordinate[1][0], resultcoordinate[1][1], result_d);
     }
 
 }
