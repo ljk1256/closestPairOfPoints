48bd89886b77bcf9b8a2ae8eebd3d0a6eb83aba5
jongmine <joychoe21@icloud.com>
2021-03-29 23:35:40 +0900

    version 2.2

--- Closest_Pair/src/Main.java
+++ Closest_Pair/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.Scanner;
 import java.util.Random;
 
@@ -5,21 +6,33 @@ public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
         int number_of_points = scanner.nextInt();
-        int[][] coordinates = new int[number_of_points][2];
 
-        // 최대 50의 정수 난수를 좌표 (x,y)에 대입
+        Main main = new Main();
+        start(number_of_points);
+
+    }
+
+    public static void start(int number_of_points){
+        Random random = new Random();
+
+        int[][] coordinates = new int[number_of_points][2];
+        int[][] resultcoordinate = new int[2][2];
+        double result_d;
+
         for (int i = 0; i < coordinates.length; i++) {
             coordinates[i][0] = random.nextInt(50);
             coordinates[i][1] = random.nextInt(50);
         }
+        Arrays.sort(coordinates);
 
-        Main closest_pair = new Main();
-        closest_pair.start(coordinates);
+        resultcoordinate = divideHalf(coordinates, 0, number_of_points);    // 최종 좌표 배열
+        result_d = get_distance(resultcoordinate, 0, 1);      // 최종 좌표 거리
+
+        System.out.println("좌표는 (%d, %d) , (%d, %d) 거리는 %f 입니다. ", resultcoordinate[0][0], resultcoordinate[0][1], resultcoordinate[1][0], resultcoordinate[1][1], result_d);
     }
 
-    public void divideHalf(int[][] coordinates, int left, int right) {
+    public static void divideHalf(int[][] coordinates, int left, int right) {
         int[][] Answer = new int[2][2];
 
 
@@ -30,7 +43,8 @@ public class Main {
             Answer = smallest_center_points.clone();
     }
 
-    public int[][] find_smallest_in_center(int[][] coordinates, int[] center_point, double max_distance) {
+    // 중앙에서 최소 거리를 이루는 점의 쌍이 있는지 찾기
+    public static int[][] find_smallest_in_center(int[][] coordinates, int[] center_point, double max_distance) {
         double smallest_distance = max_distance;  // 최소 거리
         int[][] smallest_points = new int[][]{{9999, 9999}, {9999, 9999}};  // 최소 거리를 이루는 좌표쌍
 
